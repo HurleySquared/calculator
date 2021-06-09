a06c60bf8445529a0e547e60dc33147f2d083887
Eric Hurley <erichurley421@gmail.com>
2021-06-09 14:26:15 -0400

    added dataset keytype variable

--- script.js
+++ script.js
@@ -15,3 +15,40 @@ const multiply = (a, b) => {
 };
 
 // Click event for buttons 
+const display = document.querySelector(".display");
+const keys = document.querySelector(".keys");
+const calculator = document.querySelector('.shell');
+
+keys.addEventListener('click', e => {
+  if (e.target.matches('button')) {
+  const key = e.target;
+  const action = key.dataset.action;
+  const keyContent = key.textContent;
+  const displayedNum = display.textContent;
+  const previousKeyType = calculator.dataset.previousKeyType;
+  if (!action) {
+    if (displayedNum === '0' || previousKeyType === "operator") {
+      display.textContent = keyContent
+    } else {
+      display.textContent = displayedNum + keyContent;
+    };
+  }
+
+  if (action === 'decimal') {
+    display.textContent = displayedNum + '.'
+  }
+
+  if (
+    action === 'add' ||
+    action === 'subtract' ||
+    action === 'divide' ||
+    action === 'multiply'
+  ) {
+    key.classList.add('is-depressed')
+    calculator.dataset.previousKeyType = 'operator'
+  };
+
+  Array.from(key.parentNode.children)
+    .forEach(k => k.classList.remove('is-depressed'))
+  }
+})
